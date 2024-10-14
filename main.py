3c263234b16eea5db4f23e9c98a58062d421ebd7
bugston <bugstonn@gmail.com>
2024-10-14 16:43:06 +0300

    grid buttons can be highlited, uncolored buttons are transparent and return to transparent after deselecting, when checking if button is uncolored should use if selectedbutton in COLORED_BUTTONS instead of color != ''

--- main.py
+++ main.py
@@ -8,25 +8,15 @@ import random
 from kivy.uix.image import Image
 from kivy.uix.relativelayout import RelativeLayout
 
-COLOR_BUTTONS = ['icons/blue.png', 'icons/green.png', 'icons/orange.png', 'icons/pink.png', 'icons/purple.png','icons/turquoise.png', 'icons/yellow.png']
+COLOR_BUTTONS = ['icons/blue.png', 'icons/green.png', 'icons/orange.png', 'icons/pink.png', 'icons/purple.png', 'icons/turquoise.png', 'icons/yellow.png']
 BACKGR = 'icons/background.png'
+
 class MyGameApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.score = 0
-
-    def build(self):
-        Window.size = (600, 1050)
-        parent = RelativeLayout()
-        parent.add_widget(Image(source=BACKGR, fit_mode='cover'))
-        main_layout = BoxLayout(orientation='vertical')    
-        top_layout = self.create_top_layout()
-        color_grid_layout = self.create_color_grid_layout() 
-        main_layout.add_widget(top_layout)
-        main_layout.add_widget(BoxLayout(size_hint_y=0.1))
-        main_layout.add_widget(color_grid_layout)
-        parent.add_widget(main_layout)
-        return parent
+        self.selected_button = None
+        self.grid_buttons = []
 
     def create_top_layout(self):
         top_layout = BoxLayout(orientation='horizontal', size_hint_y=0.1, padding=[10, 10, 10, 10])
@@ -42,14 +32,7 @@ class MyGameApp(App):
         top_layout.add_widget(score_button) 
         return top_layout
 
-    def update_font_size(self, label):
-        window_height = Window.size[1]
-        label.font_size = window_height * 0.09
-
-    def on_size(self, *args):
-        self.update_font_size(self.score_label)
-
-    def create_color_grid_layout(self):
+    def create_grids_layout(self):
         color_buttons_layout = self.create_color_buttons_layout()
         grid_layout = self.create_grid_layout()
         color_grid_layout = BoxLayout(orientation='vertical', size_hint=(1, 0.6))
@@ -66,24 +49,61 @@ class MyGameApp(App):
         return color_buttons_layout
 
     def create_grid_layout(self):
-        grid_layout = GridLayout(cols=9, rows=9, size_hint=(1, 0.6))   
-        for i in range(81):
-            button = Button(size_hint=(1, 1))
-            grid_layout.add_widget(button)
+        grid_layout = GridLayout(cols=9, rows=9, size_hint=(1, 0.6), spacing=4)
+        for row in range(9):
+            for col in range(9):
+                button = Button(size_hint=(1, 1))
+                button.background_color = [0, 0, 0, 0.5]
+                grid_layout.add_widget(button)
+                self.grid_buttons.append(button)
+                button.bind(on_press=self.on_button_click)
         return grid_layout
+    
+    def on_button_click(self, button):
+        if self.selected_button:
+            if self.selected_button.background_normal in COLOR_BUTTONS:
+                self.selected_button.background_color = [1, 1, 1, 1]
+            else:
+                self.selected_button.background_color = [0, 0, 0, 0.5]
+        self.selected_button = button
+        if self.selected_button.background_normal in COLOR_BUTTONS:
+            self.selected_button.background_color = [1.5, 1.5, 1.5, 1]
+        else:
+            self.selected_button.background_color = [1, 1, 1, 0.5]
+        self.assign_random_colors_to_buttons()
 
-    def get_color(self, color_name):
-        if color_name == 'red':
-            return (1, 0, 0, 1)
-        elif color_name == 'green':
-            return (0, 1, 0, 1)
-        elif color_name == 'blue':
-            return (0, 0, 1, 1)
-        return (1, 1, 1, 1)
+    def assign_random_colors_to_buttons(self):
+        selected_buttons = random.sample(self.grid_buttons, 3)
+        selected_colors = random.sample(COLOR_BUTTONS, 3)
+        for button, color in zip(selected_buttons, selected_colors):
+            button.background_normal = color
+            button.background_down = color
+            button.background_color = [1, 1, 1, 1]      
 
     def increase_score(self, instance):
         self.score += 1
         self.score_label.text = f'{self.score:04d}'
+
+    def update_font_size(self, label):
+        window_height = Window.size[1]
+        label.font_size = window_height * 0.09
+
+    def on_size(self, *args):
+        self.update_font_size(self.score_label)
+
+    def build(self):
+        Window.size = (600, 1050)
+        parent = RelativeLayout()
+        parent.add_widget(Image(source=BACKGR, fit_mode='cover'))
+        main_layout = BoxLayout(orientation='vertical')    
+        top_layout = self.create_top_layout()
+        color_grid_layout = self.create_grids_layout() 
+        main_layout.add_widget(top_layout)
+        main_layout.add_widget(BoxLayout(size_hint_y=0.1))
+        main_layout.add_widget(color_grid_layout)
+        parent.add_widget(main_layout)
+        self.assign_random_colors_to_buttons()
+        return parent
 
 if __name__ == '__main__':
     MyGameApp().run()
