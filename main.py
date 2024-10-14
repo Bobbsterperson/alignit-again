8633ae1a4fae7903056df64e0eef8b7ad456f474
bugston <bugstonn@gmail.com>
2024-10-14 12:53:56 +0300

    fixed sizing and styling to be easy to add original images in the future

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.uix.button import Button
+import random
+from kivy.uix.image import Image
+from kivy.uix.relativelayout import RelativeLayout
 
 COLOR_BUTTONS = ['icons/blue.png', 'icons/green.png', 'icons/orange.png', 'icons/pink.png', 'icons/purple.png','icons/turquoise.png', 'icons/yellow.png']
-
+BACKGR = 'icons/background.png'
 class MyGameApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -14,13 +17,16 @@ class MyGameApp(App):
 
     def build(self):
         Window.size = (600, 1050)
-        main_layout = BoxLayout(orientation='vertical')
+        parent = RelativeLayout()
+        parent.add_widget(Image(source=BACKGR, fit_mode='cover'))
+        main_layout = BoxLayout(orientation='vertical')    
         top_layout = self.create_top_layout()
         color_grid_layout = self.create_color_grid_layout() 
         main_layout.add_widget(top_layout)
         main_layout.add_widget(BoxLayout(size_hint_y=0.1))
         main_layout.add_widget(color_grid_layout)
-        return main_layout
+        parent.add_widget(main_layout)
+        return parent
 
     def create_top_layout(self):
         top_layout = BoxLayout(orientation='horizontal', size_hint_y=0.1, padding=[10, 10, 10, 10])
@@ -53,17 +59,16 @@ class MyGameApp(App):
 
     def create_color_buttons_layout(self):
         color_buttons_layout = BoxLayout(orientation='horizontal', size_hint_y=0.2, spacing=10, padding=[10, 80, 230, 10])
-        colors = ['red', 'green', 'blue']     
-        for color in colors:
-            color_button = Button(text='', background_color=self.get_color(color), size_hint=(0.05, 1))
-            color_button.bind(on_press=lambda instance, c=color: self.on_color_button_press(c))
+        selected_colors = random.sample(COLOR_BUTTONS, 3)
+        for color in selected_colors:
+            color_button = Button(background_normal=color, size_hint=(0.05, 1))
             color_buttons_layout.add_widget(color_button)
         return color_buttons_layout
 
     def create_grid_layout(self):
         grid_layout = GridLayout(cols=9, rows=9, size_hint=(1, 0.6))   
         for i in range(81):
-            button = Button(text=f'{i + 1}', size_hint=(1, 1))
+            button = Button(size_hint=(1, 1))
             grid_layout.add_widget(button)
         return grid_layout
 
@@ -79,9 +84,6 @@ class MyGameApp(App):
     def increase_score(self, instance):
         self.score += 1
         self.score_label.text = f'{self.score:04d}'
-
-    def on_color_button_press(self, color):
-        print(f'{color.capitalize()} button pressed!')
 
 if __name__ == '__main__':
     MyGameApp().run()
