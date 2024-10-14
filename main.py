95fa7cc3c566089ce18ec2e38f9d6bee627b1a65
bugston <bugstonn@gmail.com>
2024-10-14 12:27:43 +0300

    fixed relativ sizing of objects

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.uix.button import Button
 
+COLOR_BUTTONS = ['icons/blue.png', 'icons/green.png', 'icons/orange.png', 'icons/pink.png', 'icons/purple.png','icons/turquoise.png', 'icons/yellow.png']
+
 class MyGameApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.score = 0
 
     def build(self):
-        Window.size = (600, 900)
+        Window.size = (600, 1050)
         main_layout = BoxLayout(orientation='vertical')
         top_layout = self.create_top_layout()
         color_grid_layout = self.create_color_grid_layout() 
@@ -22,16 +24,24 @@ class MyGameApp(App):
 
     def create_top_layout(self):
         top_layout = BoxLayout(orientation='horizontal', size_hint_y=0.1, padding=[10, 10, 10, 10])
-        reset_button = Button(text='Reset', size_hint=(0.15, 1))
-        save_exit_button = Button(text='Save/Exit', size_hint=(0.15, 1))
-        self.score_label = Label(text='0000', size_hint=(0.15, 1))
-        score_button = Button(text='Score', size_hint=(0.15, 1))
+        reset_button = Button(background_normal='icons/restart.png', size_hint=(0.10, 1))
+        save_exit_button = Button(background_normal='icons/savexit.png', size_hint=(0.10, 1))
+        self.score_label = Label(text='0000', size_hint=(0.25, 1))
+        self.update_font_size(self.score_label)
+        score_button = Button(background_normal='icons/score.png', size_hint=(0.10, 1))
         score_button.bind(on_press=self.increase_score)     
         top_layout.add_widget(reset_button)
         top_layout.add_widget(save_exit_button)
         top_layout.add_widget(self.score_label)
         top_layout.add_widget(score_button) 
         return top_layout
+
+    def update_font_size(self, label):
+        window_height = Window.size[1]
+        label.font_size = window_height * 0.09
+
+    def on_size(self, *args):
+        self.update_font_size(self.score_label)
 
     def create_color_grid_layout(self):
         color_buttons_layout = self.create_color_buttons_layout()
@@ -42,10 +52,10 @@ class MyGameApp(App):
         return color_grid_layout
 
     def create_color_buttons_layout(self):
-        color_buttons_layout = BoxLayout(orientation='horizontal', size_hint_y=0.1, spacing=10, padding=[10, 10, 250, 10])
+        color_buttons_layout = BoxLayout(orientation='horizontal', size_hint_y=0.2, spacing=10, padding=[10, 80, 230, 10])
         colors = ['red', 'green', 'blue']     
         for color in colors:
-            color_button = Button(text='', background_color=self.get_color(color), size_hint=(5, 1))
+            color_button = Button(text='', background_color=self.get_color(color), size_hint=(0.05, 1))
             color_button.bind(on_press=lambda instance, c=color: self.on_color_button_press(c))
             color_buttons_layout.add_widget(color_button)
         return color_buttons_layout
